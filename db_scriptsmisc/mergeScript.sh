c923f62c59eafc14db08194af1cf883e1d77342b
Gabe Sargeant <gabe.sargeant@gmail.com>
2017-01-27 02:28:37 +1100

    database specific scripts and files more in the readme

---
+++ db_scriptsmisc/mergeScript.sh
@@ -0,0 +1,17 @@
+paste 2011Census_B04A* 2011Census_B04B*   > 2011Census_B04_AUST_UCL_short.csv
+paste 2011Census_B08A* 2011Census_B08B*   > 2011Census_B08_AUST_UCL_short.csv
+paste 2011Census_B10A* 2011Census_B10B* 2011Census_B10C*   > 2011Census_B10_AUST_UCL_short.csv
+paste 2011Census_B11A* 2011Census_B11B*   > 2011Census_B11_AUST_UCL_short.csv
+paste 2011Census_B12A* 2011Census_B12B*   > 2011Census_B12_AUST_UCL_short.csv
+paste 2011Census_B16A* 2011Census_B16B*   > 2011Census_B16_AUST_UCL_short.csv
+paste 2011Census_B17A* 2011Census_B17B*   > 2011Census_B17_AUST_UCL_short.csv
+paste 2011Census_B20A* 2011Census_B20B*   > 2011Census_B20_AUST_UCL_short.csv
+paste 2011Census_B22A* 2011Census_B22B*   > 2011Census_B22_AUST_UCL_short.csv
+paste 2011Census_B23A* 2011Census_B23B*   > 2011Census_B23_AUST_UCL_short.csv
+paste 2011Census_B40A* 2011Census_B40B*   > 2011Census_B40_AUST_UCL_short.csv
+paste 2011Census_B41A* 2011Census_B41B* 2011Census_B41C*   > 2011Census_B41_AUST_UCL_short.csv
+paste 2011Census_B42A* 2011Census_B42B*   > 2011Census_B42_AUST_UCL_short.csv
+paste 2011Census_B43A* 2011Census_B43B* 2011Census_B43C* 2011Census_B43D*   > 2011Census_B43_AUST_UCL_short.csv
+paste 2011Census_B44A* 2011Census_B44B*   > 2011Census_B44_AUST_UCL_short.csv
+paste 2011Census_B45A* 2011Census_B45B*   > 2011Census_B45_AUST_UCL_short.csv
+
